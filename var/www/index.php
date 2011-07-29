7b8a0ba31a33f9445723d56e3f2b376df11c7628
Arnaud Héritier <aheritier@apache.org>
2011-07-29 16:35:08 +0200

    HTML auto refresh 30s

--- var/www/index.php
+++ var/www/index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta http-equiv="refresh" content="30">
 <title>Acceptance Live Instances</title>
 <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
 <link href="/style.css" media="screen" rel="stylesheet" type="text/css"/>
