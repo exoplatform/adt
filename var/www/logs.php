d6986f03713a57e82210ccdcc4886ebf32af3c9f
Arnaud Héritier <aheritier@apache.org>
2011-08-02 00:38:04 +0200

    Improve logs viewer

--- var/www/logs.php
+++ var/www/logs.php
@@ -1,7 +1,8 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <?php
-
 $file = $_GET['file'];
-$num_latest = 5; //the number of errors to show in the "Last Few Errors" section
+$num_latest = 30; //the number of errors to show in the "Last Few Errors" section
 $allow_show_source = 1; //whether to allow the ability to view the source code of your php files
 
 //Each element in the filter array will allow you to narrow down the results by whether or not this word
@@ -13,55 +14,39 @@ $allow_show_source = 1; //whether to allow the ability to view the source code o
 //================================================================================================
 //================================================================================================
 ?>
-<html>
-<body>
 <head>
-<title>PHP Error Log Digested</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<title>Acceptance Live Instances</title>
+<link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
+<link href="/style.css" media="screen" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-<style type="text/css">
-html,body,div,p {margin:0px;padding:0px;}
-p { white-space: nowrap }
-.instructions { padding: 5px; margin-bottom: 10px; }
-.instructions p { font-size: 0.8em;}
-.latest { border-top: 1px solid #666; border-bottom: 1px solid #666; background-color: #eee; padding: 5px;}
-.latest p { font-size: 0.8em; }
-.errors { border-top: 1px solid #600; border-bottom: 1px solid #600; background-color: #fee; padding: 5px;}
-.errors b { color: #600; } 
-.errors p { font-size: 1.1em; } 
-.warnings { border-top: 1px solid #660; border-bottom: 1px solid #660; background-color: #ffe; padding: 5px;}
-.warnings b { color: #660; } 
-.notices { border-top: 1px solid #666; border-bottom: 1px solid #666; background-color: #eee; padding: 5px;}
-.notices p { font-size: 0.9em; }
-.code { font-size: 0.8em; background-color: #eee; border-top: 1px solid #666; border-bottom: 1px solid #666; padding: 5px; font-family: Courier,monospace; }
-code {font-family: Courier,monospace; }
-input,select { margin-left: 5px; }
-a { color: #000; }
-a:hover { color: #009; }
-</style>
-<?php if(empty($_GET['display_error'])) { ?>
-<div class="instructions"> <b><a href="http://www.jakeo.com/software/logdigester/index.php">Log Digester</a> Output</b> from
-  <?=$file?>
-</div>
-<?php
+<div class="UIForgePages">
+  <div class="Header ClearFix"> <a href="/" class="Logo"></a><span class="AddressWeb">acceptance.exoplatform.org</span> </div>
+  <div class="MainContent">
+    <div class="TitleForgePages">Acceptance Live Instances</div>
+    <div>
+      <?php if(empty($_GET['display_error'])) { ?>
+      <div class="instructions">File : <?=$file?></div>
+      <?php
 	if(!empty($filter)) {
 		?>
-<form action="logs.php" method="get">
-  <select name='f'>
-    <option value="">NONE</option>
-    <?php
+      <form action="logs.php" method="get">
+        <select name='f'>
+          <option value="">NONE</option>
+          <?php
 		foreach($filter as $f) {
 			?>
-    <option value="<?=$f?>">
-    <?=$f?>
-    </option>
-    <?php
+          <option value="<?=$f?>">
+          <?=$f?>
+          </option>
+          <?php
 		}
 		?>
-  </select>
-  <input type="submit" value="Filter" />
-</form>
-<?php
+        </select>
+        <input type="submit" value="Filter" />
+      </form>
+      <?php
 	}
 	
 	function mysort($a,$b) {
@@ -73,6 +58,7 @@ a:hover { color: #009; }
 	
 	$handle = @fopen($file,"r");
 	if($handle) {
+	   $linenumber = 0;
 	   while(!feof($handle)) {
 			$line = fgets($handle, 4096); //get line
 			
@@ -81,21 +67,22 @@ a:hover { color: #009; }
 				$latest[] = $line;
 				if(sizeof($latest)>1+$num_latest) array_shift($latest);
 				
-				$line = ereg_replace(".* PHP ","",$line); //gets rid of timestamp
+				//$line = ereg_replace(".* PHP ","",$line); //gets rid of timestamp
 				
 				//gets line number of error		
-				ereg(" on line ([0-9]*)",$line,$linenumber); 
-				$linenumber = $linenumber[1];
-				if(empty($linenumber)) $linenumber=" ";
-				$line = ereg_replace(" on line [0-9]*","",$line);
+				//ereg(" on line ([0-9]*)",$line,$linenumber); 
+				//$linenumber = $linenumber[1];
+				//if(empty($linenumber)) $linenumber=" ";
+				//$line = ereg_replace(" on line [0-9]*","",$line);
 				
+				$linenumber = $linenumber + 1;
 				$hash = md5($line); //make a unique id for this error
 				
 				//gets filepath
-				ereg(" in ([^ ]*)",$line,$filepath); 
-				$filepath = trim($filepath[1]);
-				if(empty($filepath)) $filepath="";
-				$line = ereg_replace(" in [^ ]*","",$line);
+				//ereg(" in ([^ ]*)",$line,$filepath); 
+				//$filepath = trim($filepath[1]);
+				//if(empty($filepath)) $filepath="";
+				//$line = ereg_replace(" in [^ ]*","",$line);
 				
 				//figures out severity of error
 				$severity=3; 
@@ -110,14 +97,14 @@ a:hover { color: #009; }
 						$res[$severity][$hash][0]=1;
 						$res[$severity][$hash][1]=$line;
 						if(empty($allow_show_source)) $res[$severity][$hash][2]=$linenumber;
-						else $res[$severity][$hash][2]="<a href='logs.php?display_error=".urlencode($filepath)."&amp;line=".$linenumber."#jump'>".$linenumber."</a>";
+						else $res[$severity][$hash][2]="<a href='logs.php?file=".urlencode($filepath)."&amp;line=".$linenumber."#jump'>".$linenumber."</a>";
 						$res[$severity][$hash][3]=$filepath;
 						
 					} else {
 						$res[$severity][$hash][0]++; //repeat error, so increment the existsing value
 						if(strstr($res[$severity][$hash][2],$linenumber)==FALSE) {
 							if(empty($allow_show_source)) $res[$severity][$hash][2].=" ".$linenumber;
-							else $res[$severity][$hash][2].=" <a href='logs.php?display_error=".urlencode($filepath)."&amp;line=".$linenumber."#jump'>".$linenumber."</a>";
+							else $res[$severity][$hash][2].=" <a href='logs.php?file=".urlencode($filepath)."&amp;line=".$linenumber."#jump'>".$linenumber."</a>";
 						}
 					}
 				}
@@ -175,19 +162,14 @@ a:hover { color: #009; }
 		echo "Couldn't read error file.";
 	}
 	?>
-<br />
-<br />
-<b>Key:</b> The first number is the frequency count (bigger number=worse error).  This is followed by the error.  The numbers at the ends are the line numbers at which the errors have occurred in your php file. <br />
-<?php } else if(!empty($allow_show_source)) { ?>
-<div class="instructions"> <b><a href="http://www.jakeo.com/software/logdigester/index.php">Log Digester</a> Output</b> from <a href="logs.php">
-  <?=$file?>
-  </a><br />
-  Showing errors from:
-  <?=$_GET['display_error']?>
-</div>
-<div class="code">
-  <?php
-		$output = highlight_file($_GET['display_error'], true);
+      <br />
+      <br />
+      <b>Key:</b> The first number is the frequency count (bigger number=worse error).  This is followed by the error.  The numbers at the ends are the line numbers at which the errors have occurred in your file. <br />
+      <?php } if(!empty($allow_show_source)) { ?>
+      <div class="instructions">File : <?=$file?></div>
+      <div class="code">
+        <?php
+		$output = highlight_file($_GET['file'], true);
 	
 		//Line breaks are a little strange
 		// dos = \r\n
@@ -201,16 +183,20 @@ a:hover { color: #009; }
 			if($i+1==$_GET['line']) echo "<a name='jump'></a><font color='black'>***</font>&nbsp;";
 			else echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 			?>
-  <font color="black">
-  <?=$i+1?>
-  </font>&nbsp;&nbsp;&nbsp;
-  <?=$lines[$i]?>
-  <br />
-  <?php
+        <font color="black">
+        <?=$i+1?>
+        </font>&nbsp;&nbsp;&nbsp;
+        <?=$lines[$i]?>
+        <br />
+        <?php
 		}
 		
 	?>
+      </div>
+      <? } ?>
+    </div>
+  </div>
+  <div class="Footer">eXo Platform SAS</div>
 </div>
-<? } ?>
 </body>
 </html>
