7fc47febe93702722ce7bb6a276b31a6a0f11433
Arnaud Héritier <aheritier@apache.org>
2011-08-05 23:25:43 +0200

    SWF-1398 : Remove compression on reverse proxy (I'm not enough confident on it)

--- adt.sh
+++ adt.sh
@@ -683,30 +683,6 @@ cat << EOF > $APACHE_CONF_DIR/$PRODUCT_NAME-$PRODUCT_VERSION.acceptance.exoplatf
         Allow from all
     </Directory>
     
-    #
-    # Compression using GZIP
-    #
-    # Insert filter
-    SetOutputFilter DEFLATE
-    SetInputFilter DEFLATE
-    DeflateFilterNote Input instream
-    DeflateFilterNote Output outstream
-    DeflateFilterNote Ratio ratio
-    # Higher Compression 9 - Medium 5
-    DeflateCompressionLevel 5
-
-    # Netscape 4.x has some problems...
-    BrowserMatch ^Mozilla/4 gzip-only-text/html
-    # Netscape 4.06-4.08 have some more problems
-    BrowserMatch ^Mozilla/4\.0[678] no-gzip
-    # MSIE masquerades as Netscape, but it is fine
-    BrowserMatch \bMSIE !no-gzip !gzip-only-text/html
-    
-    # Don't compress images
-    SetEnvIfNoCase Request_URI "\.(?:gif|jpe?g|png)\$" no-gzip dont-vary
-    # Make sure proxies don't deliver the wrong content
-    Header append Vary User-Agent env=!dont-vary
-    
     ProxyRequests           Off
     ProxyPreserveHost       On
     ProxyPass               /exo-static/   !
