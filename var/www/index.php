028b11dc5f77dabce3c5573b2dfbfcf2c7ef938f
Arnaud Héritier <aheritier@exoplatform.com>
2011-11-18 10:18:04 +0100

    Bye bye beta

--- var/www/index.php
+++ var/www/index.php
@@ -23,7 +23,6 @@
 <div class="UIForgePages">
   <div class="Header ClearFix"> <a href="/" class="Logo"></a><span class="AddressWeb">acceptance.exoplatform.org</span> </div>
   <div class="MainContent">
-    <div class="Ribbon">Beta !</div>
     <div class="TitleForgePages">Acceptance Live Instances</div>
     <div>
       <div>
