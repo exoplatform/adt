37c5d4f9390aefc6e34e37f2c46297a0befdee80
Arnaud Héritier <aheritier@exoplatform.com>
2012-10-03 23:34:39 +0200

    SWF-2158 : DON'T USE CURL

--- var/www/index.php
+++ var/www/index.php
@@ -76,41 +76,10 @@ header("Pragma: no-cache"); // HTTP/1.0
 		          </thead>
               <tbody>
 		            <?php
-		          // Get remote file contents, preferring faster cURL if available
-		          function remote_get_contents($url)
-		          {
-		            if (function_exists('curl_get_contents') AND function_exists('curl_init'))
-		            {
-		              return curl_get_contents($url);
-		            }
-		            else
-		            {
-		              // A litte slower, but (usually) gets the job done
-		              return file_get_contents($url);
-		            }
-		          }
-
-		          function curl_get_contents($url)
-		          {
-		            // Initiate the curl session
-		            $ch = curl_init();
-		            // Set the URL
-		            curl_setopt($ch, CURLOPT_URL, $url);
-		            // Removes the headers from the output
-		            curl_setopt($ch, CURLOPT_HEADER, 0);
-		            // Return the output instead of displaying it directly
-		            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		            // Execute the curl session
-		            $output = curl_exec($ch);
-		            // Close the curl session
-		            curl_close($ch);
-		            // Return the output as a variable
-		            return $output;
-		          }
 		          function append_data($url,$data)
 		          {
 		            $result=$data;
-		            $values=(array)json_decode(remote_get_contents($url));
+		            $values=(array)json_decode(file_get_contents($url));
 		            while ($entry = current($values)) {
 									$key=key($values);
 		              if(!array_key_exists($key,$data)){
