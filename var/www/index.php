b78fca43f6fe27ad4acb3b368ba79ccfc9a777ea
Arnaud Héritier <aheritier@exoplatform.com>
2012-10-03 16:22:23 +0200

    SWF-2158 : Fix download remote json

--- var/www/index.php
+++ var/www/index.php
@@ -53,7 +53,38 @@
           </thead>
           <tbody>
             <?php
-					$list = array_merge(json_decode(file_get_contents('http://acceptance.exoplatform.org/list.php')),json_decode(file_get_contents('http://acceptance2.exoplatform.org/list.php')));
+					// Get remote file contents, preferring faster cURL if available
+					function remote_get_contents($url)
+					{
+					        if (function_exists('curl_get_contents') AND function_exists('curl_init'))
+					        {
+					                return curl_get_contents($url);
+					        }
+					        else
+					        {
+					                // A litte slower, but (usually) gets the job done
+					                return file_get_contents($url);
+					        }
+					}
+
+					function curl_get_contents($url)
+					{
+					        // Initiate the curl session
+					        $ch = curl_init();
+					        // Set the URL
+					        curl_setopt($ch, CURLOPT_URL, $url);
+					        // Removes the headers from the output
+					        curl_setopt($ch, CURLOPT_HEADER, 0);
+					        // Return the output instead of displaying it directly
+					        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					        // Execute the curl session
+					        $output = curl_exec($ch);
+					        // Close the curl session
+					        curl_close($ch);
+					        // Return the output as a variable
+					        return $output;
+					}
+          $list = array_merge(json_decode(remote_get_contents('http://acceptance.exoplatform.org/list.php')),json_decode(remote_get_contents('http://acceptance2.exoplatform.org/list.php')));
           sort($list);
           foreach( $list as $descriptor_array) {
             ?>
