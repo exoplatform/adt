48521d711c8b38beaeed2c650848ccabede38164
Arnaud Héritier <aheritier@exoplatform.com>
2012-10-03 16:50:37 +0200

    SWF-2158 : Fix message

--- var/www/index.php
+++ var/www/index.php
@@ -91,7 +91,7 @@
             <tr onmouseover="this.className='normalActive'" onmouseout="this.className='normal'" class="normal">
               <td><?=strtoupper($descriptor_array->PRODUCT_NAME)?></td>
               <td><?=$descriptor_array->PRODUCT_VERSION?></td>
-              <td><a href="<?=$descriptor_array->ARTIFACT_DL_URL?>" class="TxtBlue" title="Download <?=$descriptor_array->ARTIFACT_GROUPID?>:<?=$descriptor_array->ARTIFACT_ARTIFACTID?>:<?=$descriptor_array->ARTIFACT_TIMESTAMP?> from Nexus"><img class="left" src="/images/ButDownload.gif" alt="Download" width="19" height="19" />&nbsp;<?=$descriptor_array->ARTIFACT_TIMESTAMP?></a></td>
+              <td><a href="<?=$descriptor_array->ARTIFACT_DL_URL?>" class="TxtBlue" title="Download <?=$descriptor_array->ARTIFACT_GROUPID?>:<?=$descriptor_array->ARTIFACT_ARTIFACTID?>:<?=$descriptor_array->ARTIFACT_TIMESTAMP?> from Acceptance"><img class="left" src="/images/ButDownload.gif" alt="Download" width="19" height="19" />&nbsp;<?=$descriptor_array->ARTIFACT_TIMESTAMP?></a></td>
               <td class="<?=$descriptor_array->ARTIFACT_AGE_CLASS?>"><?=$descriptor_array->ARTIFACT_AGE_STRING?></td>
               <td><?php if( $descriptor_array->DEPLOYMENT_ENABLED ) { echo $descriptor_array->DEPLOYMENT_AGE_STRING; } ?></td>
               <td><?php if( $descriptor_array->DEPLOYMENT_ENABLED ) { ?><a href="<?=$descriptor_array->DEPLOYMENT_URL?>" class="TxtBlue" target="_blank" title="Open the instance in a new window"><?=$descriptor_array->DEPLOYMENT_URL?></a><?php } ?></td>
