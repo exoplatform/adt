f0a088beaa11dd5bb6fef66af45a17c1d7ff67bf
Arnaud Héritier <aheritier@exoplatform.com>
2012-10-04 15:07:25 +0200

    mod_expires manages cache on apache side

--- var/www/index.php
+++ var/www/index.php
@@ -1,18 +1,4 @@
 <!DOCTYPE html>
-<?php
-/*
- No cache!!
-*/
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-// always modified
-header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache"); // HTTP/1.0
-/*
- End of No cache
-*/
-?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
