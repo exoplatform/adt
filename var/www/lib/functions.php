7a9b339573500d85d2d85669a4dc211e58aa2722
Arnaud Héritier <aheritier@exoplatform.com>
2013-01-04 00:52:44 +0100

    Move load instances details into a function Use a cache (using APC) for projects, branches, instances

--- var/www/lib/functions.php
+++ var/www/lib/functions.php
@@ -109,39 +109,133 @@ function sortProjects($a, $b)
 
 function getProjects()
 {
-    //List all repos
-    $projects = preg_replace('/\.git/', '', getGitDirectoriesList(getenv('ADT_DATA') . "/sources/"));
-    usort($projects, "sortProjects");
+    $projects = apc_fetch('projects');
+
+    if (empty($projects)) {
+        //List all repos
+        $projects = preg_replace('/\.git/', '', getGitDirectoriesList(getenv('ADT_DATA') . "/sources/"));
+        usort($projects, "sortProjects");
+
+        // Projects will be cached for 1 hour
+        apc_store('projects', $projects, 3600);
+    }
     return $projects;
 }
 
 function getFeatureBranches($projects)
 {
-    $features = array();
-    foreach ($projects as $project) {
-        $repoObject = new PHPGit_Repository(getenv('ADT_DATA') . "/sources/" . $project . ".git");
-        $branches = array_filter(preg_replace('/.*\/feature\//', '', array_filter(explode("\n", $repoObject->git('branch -r')), 'isFeature')));
-        foreach ($branches as $branch) {
-            $fetch_url = $repoObject->git('config --get remote.origin.url');
-            if (preg_match("/git:\/\/github\.com\/(.*)\/(.*)\.git/", $fetch_url, $matches)) {
-                $github_org = $matches[1];
-                $github_repo = $matches[2];
+    $features = apc_fetch('features');
+
+    if (empty($features)) {
+        $features = array();
+        foreach ($projects as $project) {
+            $repoObject = new PHPGit_Repository(getenv('ADT_DATA') . "/sources/" . $project . ".git");
+            $branches = array_filter(preg_replace('/.*\/feature\//', '', array_filter(explode("\n", $repoObject->git('branch -r')), 'isFeature')));
+            foreach ($branches as $branch) {
+                $fetch_url = $repoObject->git('config --get remote.origin.url');
+                if (preg_match("/git:\/\/github\.com\/(.*)\/(.*)\.git/", $fetch_url, $matches)) {
+                    $github_org = $matches[1];
+                    $github_repo = $matches[2];
+                }
+                $features[$branch][$project]['http_url'] = "https://github.com/" . $github_org . "/" . $github_repo . "/tree/feature/" . $branch;
+                $behind_commits_logs = $repoObject->git("log origin/feature/" . $branch . "..origin/master --oneline");
+                if (empty($behind_commits_logs))
+                    $features[$branch][$project]['behind_commits'] = 0;
+                else
+                    $features[$branch][$project]['behind_commits'] = count(explode("\n", $behind_commits_logs));
+                $ahead_commits_logs = $repoObject->git("log origin/master..origin/feature/" . $branch . " --oneline");
+                if (empty($ahead_commits_logs))
+                    $features[$branch][$project]['ahead_commits'] = 0;
+                else
+                    $features[$branch][$project]['ahead_commits'] = count(explode("\n", $ahead_commits_logs));
             }
-            $features[$branch][$project]['http_url'] = "https://github.com/" . $github_org . "/" . $github_repo . "/tree/feature/" . $branch;
-            $behind_commits_logs = $repoObject->git("log origin/feature/" . $branch . "..origin/master --oneline");
-            if (empty($behind_commits_logs))
-                $features[$branch][$project]['behind_commits'] = 0;
-            else
-                $features[$branch][$project]['behind_commits'] = count(explode("\n", $behind_commits_logs));
-            $ahead_commits_logs = $repoObject->git("log origin/master..origin/feature/" . $branch . " --oneline");
-            if (empty($ahead_commits_logs))
-                $features[$branch][$project]['ahead_commits'] = 0;
-            else
-                $features[$branch][$project]['ahead_commits'] = count(explode("\n", $ahead_commits_logs));
         }
+        uksort($features, 'strcasecmp');
+        // Feature branches will be cached for 5 min
+        apc_store('features', $features, 300);
     }
-    uksort($features, 'strcasecmp');
     return $features;
+}
+
+function getAcceptanceInstances()
+{
+    $instances = apc_fetch('instances');
+
+    if (empty($instances)) {
+        $instances = array();
+        $vhosts = getDirectoryList(getenv('ADT_DATA') . "/conf/adt/");
+        $now = new DateTime();
+        foreach ($vhosts as $vhost) {
+            // Parse deployment descriptor
+            $descriptor_array = parse_ini_file(getenv('ADT_DATA') . "/conf/adt/" . $vhost);
+            if ($descriptor_array['ARTIFACT_DATE']) {
+                $artifact_age = DateTime::createFromFormat('Ymd.His', $descriptor_array['ARTIFACT_DATE'])->diff($now, true);
+                if ($artifact_age->days)
+                    $descriptor_array['ARTIFACT_AGE_STRING'] = $artifact_age->format('%a day(s) ago');
+                else if ($artifact_age->h > 0)
+                    $descriptor_array['ARTIFACT_AGE_STRING'] = $artifact_age->format('%h hour(s) ago');
+                else
+                    $descriptor_array['ARTIFACT_AGE_STRING'] = $artifact_age->format('%i minute(s) ago');
+                if ($artifact_age->days > 5)
+                    $descriptor_array['ARTIFACT_AGE_CLASS'] = "red";
+                else if ($artifact_age->days > 2)
+                    $descriptor_array['ARTIFACT_AGE_CLASS'] = "orange";
+                else
+                    $descriptor_array['ARTIFACT_AGE_CLASS'] = "green";
+            } else {
+                $descriptor_array['ARTIFACT_AGE_STRING'] = "Unknown";
+                $descriptor_array['ARTIFACT_AGE_CLASS'] = "black";
+            }
+            $deployment_age = DateTime::createFromFormat('Ymd.His', $descriptor_array['DEPLOYMENT_DATE'])->diff($now, true);
+            if ($deployment_age->days)
+                $descriptor_array['DEPLOYMENT_AGE_STRING'] = $deployment_age->format('%a day(s) ago');
+            else if ($deployment_age->h > 0)
+                $descriptor_array['DEPLOYMENT_AGE_STRING'] = $deployment_age->format('%h hour(s) ago');
+            else
+                $descriptor_array['DEPLOYMENT_AGE_STRING'] = $deployment_age->format('%i minute(s) ago');
+            // Logs URLs
+            $scheme = ((!empty($_SERVER['HTTPS'])) && ($_SERVER['HTTPS'] != 'off')) ? "https" : "http";
+
+            $descriptor_array['DEPLOYMENT_LOG_APPSRV_URL'] = $scheme . "://" . $_SERVER['SERVER_NAME'] . "/logs.php?file=" . $descriptor_array['DEPLOYMENT_LOG_PATH'];
+            $descriptor_array['DEPLOYMENT_LOG_APACHE_URL'] = $scheme . "://" . $_SERVER['SERVER_NAME'] . "/logs.php?file=" . getenv('ADT_DATA') . "/var/log/apache2/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . "-access.log";
+            $descriptor_array['DEPLOYMENT_AWSTATS_URL'] = $scheme . "://" . $_SERVER['SERVER_NAME'] . "/stats/awstats.pl?config=" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'];
+            // status
+            if (file_exists($descriptor_array['DEPLOYMENT_PID_FILE']) && processIsRunning(file_get_contents($descriptor_array['DEPLOYMENT_PID_FILE'])))
+                $descriptor_array['DEPLOYMENT_STATUS'] = "Up";
+            else
+                $descriptor_array['DEPLOYMENT_STATUS'] = "Down";
+            // Acceptance process state
+            if (file_exists(getenv('ADT_DATA') . "/conf/features/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . ".status"))
+                $descriptor_array['ACCEPTANCE_STATE'] = file_get_contents(getenv('ADT_DATA') . "/conf/features/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . ".status");
+            else
+                $descriptor_array['ACCEPTANCE_STATE'] = "Implementing";
+            // Specification Link
+            if (file_exists(getenv('ADT_DATA') . "/conf/features/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . ".spec"))
+                $descriptor_array['SPECIFICATIONS_LINK'] = file_get_contents(getenv('ADT_DATA') . "/conf/features/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . ".spec");
+            else
+                $descriptor_array['SPECIFICATIONS_LINK'] = "";
+            // Issue Link
+            if (file_exists(getenv('ADT_DATA') . "/conf/features/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . ".issue"))
+                $descriptor_array['ISSUE_NUM'] = file_get_contents(getenv('ADT_DATA') . "/conf/features/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . ".issue");
+            else
+                $descriptor_array['ISSUE_NUM'] = "";
+            // SCM BRANCH
+            if (file_exists(getenv('ADT_DATA') . "/conf/features/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . ".branch"))
+                $descriptor_array['SCM_BRANCH'] = file_get_contents(getenv('ADT_DATA') . "/conf/features/" . $descriptor_array['PRODUCT_NAME'] . "-" . $descriptor_array['PRODUCT_VERSION'] . "." . $_SERVER['SERVER_NAME'] . ".branch");
+            else
+                $descriptor_array['SCM_BRANCH'] = "";
+            // Server hostname where is deployed the instance
+            $descriptor_array['ACCEPTANCE_SERVER'] = $_SERVER['SERVER_NAME'];
+            // Add it in the list
+            if (empty($descriptor_array['PLF_BRANCH']))
+                $instances['UNKNOWN'][] = $descriptor_array;
+            else
+                $instances[$descriptor_array['PLF_BRANCH']][] = $descriptor_array;
+        }
+        // Instances will be cached for 2 min
+        apc_store('instances', $instances, 20);
+    }
+    return $instances;
 }
 
 ?>
