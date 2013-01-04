93f94b9d5ea93d25874b5e172cc921dfeda3b448
Arnaud Héritier <aheritier@exoplatform.com>
2013-01-04 14:45:21 +0100

    Allow to reset a branch value to nothing

--- var/www/editFeature.php
+++ var/www/editFeature.php
@@ -2,7 +2,12 @@
 file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".spec", $_POST['specifications']);
 file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".status", $_POST['status']);
 file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".issue", $_POST['issue']);
-if ($_POST['branch'] !== "UNSET") file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".branch", $_POST['branch']);
+if ($_POST['branch'] !== "UNSET"){
+    file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".branch", $_POST['branch']);
+} else {
+    // Remove any existing value by removing the file
+    unlink(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".branch");
+}
 // Flush caches
 apc_delete("all_instances");
 apc_delete("local_instances");
