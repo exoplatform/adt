259912fb84cf3dae75484ac68cbfd31ee19d4541
Arnaud Héritier <aheritier@exoplatform.com>
2013-01-04 12:32:44 +0100

    Flush caches after edition (it won't work for all pages due to the fact that we are using a cache locally to each server)

--- var/www/editFeature.php
+++ var/www/editFeature.php
@@ -3,6 +3,9 @@ file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "
 file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".status", $_POST['status']);
 file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".issue", $_POST['issue']);
 file_put_contents(getenv('ADT_DATA') . "/conf/features/" . $_POST['product'] . "-" . $_POST['version'] . "." . $_POST['server'] . ".branch", $_POST['branch']);
+// Flush caches
+apc_delete("all_instances");
+apc_delete("local_instances");
 header("Location: " . $_POST['from']); /* Redirect browser */
 /* Make sure that code below does not get executed when we redirect. */
 exit;
