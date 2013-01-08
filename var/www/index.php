5989cc8edb563f1b6d30274b84f67a507d1755bb
Arnaud Héritier <aheritier@exoplatform.com>
2013-01-09 00:32:55 +0100

    Improve feature edition modal box for smaller resolutions

--- var/www/index.php
+++ var/www/index.php
@@ -162,7 +162,7 @@ foreach ($all_instances as $plf_branch => $descriptor_arrays) {
         </tr>
         <?php if (!empty($feature_branch)) { ?>
             <form class="form" action="http://<?=$descriptor_array->ACCEPTANCE_SERVER?>/editFeature.php" method="POST">
-                <div class="modal hide fade" id="edit-<?=$descriptor_array->PRODUCT_NAME?>-<?=str_replace(".", "_", $descriptor_array->PRODUCT_VERSION)?>" tabindex="-1" role="dialog" aria-labelledby="label-<?=$descriptor_array->PRODUCT_NAME?>-<?=$descriptor_array->PRODUCT_VERSION?>" aria-hidden="true">
+                <div class="modal bigModal hide fade" id="edit-<?=$descriptor_array->PRODUCT_NAME?>-<?=str_replace(".", "_", $descriptor_array->PRODUCT_VERSION)?>" tabindex="-1" role="dialog" aria-labelledby="label-<?=$descriptor_array->PRODUCT_NAME?>-<?=$descriptor_array->PRODUCT_VERSION?>" aria-hidden="true">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                         <h3 id="label-<?=$descriptor_array->PRODUCT_NAME?>-<?=$descriptor_array->PRODUCT_VERSION?>">Edit Feature Branch</h3>
@@ -186,83 +186,96 @@ foreach ($all_instances as $plf_branch => $descriptor_arrays) {
                             <div class="span8"><?=$feature_branch?></div>
                         </div>
                         <hr/>
-                        <div class="control-group">
-                            <label class="control-label" for="specifications"><strong>Specifications link</strong></label>
+                        <div class="row-fluid">
+                            <div class="span12">
+                                <div class="control-group">
+                                    <label class="control-label" for="specifications"><strong>Specifications link</strong></label>
 
-                            <div class="controls">
-                                <input class="input-xxlarge" type="url" id="specifications" name="specifications" placeholder="Url" value="<?=$descriptor_array->SPECIFICATIONS_LINK?>">
-                                <span class="help-block">eXo intranet URL of specifications</span>
+                                    <div class="controls">
+                                        <input class="input-xxlarge" type="url" id="specifications" name="specifications" placeholder="Url" value="<?=$descriptor_array->SPECIFICATIONS_LINK?>">
+                                        <span class="help-block">eXo intranet URL of specifications</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="control-group">
-                            <label class="control-label" for="issue"><strong>Issue key</strong></label>
+                        <div class="row-fluid">
+                            <div class="span4">
+                                <div class="control-group">
+                                    <label class="control-label" for="issue"><strong>Issue key</strong></label>
 
-                            <div class="controls">
-                                <input class="input-medium" type="text" id="issue" name="issue" placeholder="XXX-nnnn" value="<?=$descriptor_array->ISSUE_NUM?>">
-                                <span class="help-block">Issue key where testers can give a feedback.</span>
+                                    <div class="controls">
+                                        <input class="input-medium" type="text" id="issue" name="issue" placeholder="XXX-nnnn" value="<?=$descriptor_array->ISSUE_NUM?>">
+                                        <span class="help-block">Issue key where testers can give a feedback.</span>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="control-group">
-                            <label class="control-label" for="status"><strong>Status</strong></label>
+                            <div class="span4">
+                                <div class="control-group">
+                                    <label class="control-label" for="status"><strong>Status</strong></label>
 
-                            <div class="controls" id="status">
-                                <select name="status">
-                                    <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "Implementing") {
-                                        echo "selected";
-                                    }?>>Implementing
-                                    </option>
-                                    <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "Engineering Review") {
-                                        echo "selected";
-                                    }?>>Engineering Review
-                                    </option>
-                                    <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "QA Review") {
-                                        echo "selected";
-                                    }?>>QA Review
-                                    </option>
-                                    <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "QA In Progress") {
-                                        echo "selected";
-                                    }?>>QA In Progress
-                                    </option>
-                                    <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "QA Rejected") {
-                                        echo "selected";
-                                    }?>>QA Rejected
-                                    </option>
-                                    <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "Validated") {
-                                        echo "selected";
-                                    }?>>Validated
-                                    </option>
-                                    <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "Merged") {
-                                        echo "selected";
-                                    }?>>Merged
-                                    </option>
-                                </select>
-                                <span class="help-block">Current status of the feature branch</span>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <label class="control-label" for="branch"><strong>Git branch</strong></label>
-
-                            <div class="controls" id="branch">
-                                <select name="branch">
-                                    <option value="UNSET">=== Undefined ===</option>
-                                    <?php
-                                    //List all projects
-                                    $features = getFeatureBranches();
-                                    foreach ($features as $feature => $FBProjects) {
-                                        if ((!empty($descriptor_array->SCM_BRANCH) && $descriptor_array->SCM_BRANCH === $feature) || !in_array($feature, getFeatureBranches())) {
-                                            ?>
-                                            <option <?php if (!empty($descriptor_array->SCM_BRANCH) && $descriptor_array->SCM_BRANCH === $feature) {
+                                    <div class="controls" id="status">
+                                        <select name="status">
+                                            <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "Implementing") {
                                                 echo "selected";
-                                            }?>><?=$feature?>
+                                            }?>>Implementing
                                             </option>
-                                        <?php
-                                        }
-                                    }
-                                    ?>
-                                </select>
-                                <span class="help-block">Git branch hosting this development</span>
+                                            <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "Engineering Review") {
+                                                echo "selected";
+                                            }?>>Engineering Review
+                                            </option>
+                                            <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "QA Review") {
+                                                echo "selected";
+                                            }?>>QA Review
+                                            </option>
+                                            <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "QA In Progress") {
+                                                echo "selected";
+                                            }?>>QA In Progress
+                                            </option>
+                                            <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "QA Rejected") {
+                                                echo "selected";
+                                            }?>>QA Rejected
+                                            </option>
+                                            <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "Validated") {
+                                                echo "selected";
+                                            }?>>Validated
+                                            </option>
+                                            <option <?php if ($descriptor_array->ACCEPTANCE_STATE === "Merged") {
+                                                echo "selected";
+                                            }?>>Merged
+                                            </option>
+                                        </select>
+                                        <span class="help-block">Current status of the feature branch</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span4">
+                                <div class="control-group">
+                                    <label class="control-label" for="branch"><strong>Git branch</strong></label>
+
+                                    <div class="controls" id="branch">
+                                        <select name="branch">
+                                            <option value="UNSET">=== Undefined ===</option>
+                                            <?php
+                                            //List all projects
+                                            $features = getFeatureBranches();
+                                            foreach ($features as $feature => $FBProjects) {
+                                                if ((!empty($descriptor_array->SCM_BRANCH) && $descriptor_array->SCM_BRANCH === $feature) || !in_array($feature, getFeatureBranches())) {
+                                                    ?>
+                                                    <option <?php if (!empty($descriptor_array->SCM_BRANCH) && $descriptor_array->SCM_BRANCH === $feature) {
+                                                        echo "selected";
+                                                    }?>><?=$feature?>
+                                                    </option>
+                                                <?php
+                                                }
+                                            }
+                                            ?>
+                                        </select>
+                                        <span class="help-block">Git branch hosting this development</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
                     </div>
                     <div class="modal-footer">
                         <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
