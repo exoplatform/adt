a9c14d92f9e2394282afb1476ab7bc241ba24098
Arnaud Héritier <aheritier@exoplatform.com>
2013-03-14 22:07:08 +0100

    SWF-2491 : Use UTC everywhere

--- var/www/lib/functions.php
+++ var/www/lib/functions.php
@@ -180,7 +180,6 @@ function getFeatureBranches()
 function getLocalAcceptanceInstances()
 {
     $instances = apc_fetch('local_instances');
-    $UTC = new DateTimeZone("UTC");
 
     if (empty($instances)) {
         $instances = array();
@@ -190,7 +189,7 @@ function getLocalAcceptanceInstances()
             // Parse deployment descriptor
             $descriptor_array = parse_ini_file(getenv('ADT_DATA') . "/conf/adt/" . $vhost);
             if ($descriptor_array['ARTIFACT_DATE']) {
-                $artifact_age = DateTime::createFromFormat('Ymd.His', $descriptor_array['ARTIFACT_DATE'], $UTC)->diff($now, true);
+                $artifact_age = DateTime::createFromFormat('Ymd.His', $descriptor_array['ARTIFACT_DATE'])->diff($now, true);
                 if ($artifact_age->days)
                     $descriptor_array['ARTIFACT_AGE_STRING'] = $artifact_age->format('%a day(s) ago');
                 else if ($artifact_age->h > 0)
