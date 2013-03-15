1a6e728a8312a373ee2dd742f770db54d86cbd06
Arnaud Héritier <aheritier@exoplatform.com>
2013-03-15 14:23:18 +0100

    PLFENG-1398 : Update to deploy Intranet vPLF4

--- adt.sh
+++ adt.sh
@@ -450,7 +450,15 @@ initialize_product_settings() {
           env_var PRODUCT_DESCRIPTION         "eXo Company Intranet"
           env_var ARTIFACT_REPO_GROUP         "cp"
           env_var ARTIFACT_GROUPID            "com.exoplatform.intranet"
-          env_var ARTIFACT_ARTIFACTID         "exo-intranet-package"
+          case "${PRODUCT_BRANCH}" in
+            "3.5.x")
+  	          env_var ARTIFACT_ARTIFACTID         "exo-intranet-package"
+            ;;
+            *)
+              # 4.0.x and +
+		          env_var ARTIFACT_ARTIFACTID         "company-intranet-package"
+            ;;
+          esac
           env_var DEPLOYMENT_SERVER_SCRIPT    "bin/catalina.sh"
           env_var EXO_PROFILES                "default"
           env_var DEPLOYMENT_DATABASE_TYPE    "MYSQL"
