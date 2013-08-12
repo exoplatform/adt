81e25940bbdce3588e9278c1a591ccc92e8e7290
Frederic DROUET <fdrouet@exoplatform.com>
2013-08-12 17:02:38 +0200

    Manage GateIn 3.7 version classification in Acceptance homepage

--- _functions.sh
+++ _functions.sh
@@ -263,7 +263,7 @@ initialize_product_settings() {
             "3.5.x")
               env_var PLF_BRANCH "4.0.x"
             ;;
-            "3.6.x"|"4.0.x")
+            "3.6.x"|"3.7.x"|"4.0.x")
               env_var PLF_BRANCH "4.1.x"
             ;;
           esac
