2fafceb5d2d7621bbd6fcec21517b600b38706a3
Vincent Sellier <vsellier@exoplatform.com>
2016-06-03 15:21:18 +0200

    fix alias declaration

--- _functions_aliases.sh
+++ _functions_aliases.sh
@@ -19,11 +19,15 @@ fi
 # Load shared functions
 # #############################################################################
 source "${SCRIPT_DIR}/_functions_core.sh"
+source "${SCRIPT_DIR}/_functions_system.sh"
+
+loadSystemInfo
 
 #Activate aliases usage in scripts
 shopt -s expand_aliases
 
 # Various command aliases
+echo_info "*****LINUX:${LINUX}"
 if ${LINUX}; then
   alias display_time='/usr/bin/time -f "[INFO] Return code : %x\n[INFO] Time report (sec) : \t%e real,\t%U user,\t%S system"'
 else
