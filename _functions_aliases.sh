3b263e5da7498d45c8e43f73a6f3c645e0116540
Vincent Sellier <vsellier@exoplatform.com>
2016-06-06 15:35:23 +0200

    log environment detection in debug level

--- _functions_aliases.sh
+++ _functions_aliases.sh
@@ -26,7 +26,7 @@ initAliases() {
   shopt -s expand_aliases
 
   # Various command aliases
-  echo_info "*****LINUX:${LINUX}"
+  echo_debug "Linux environment detected :${LINUX}"
   if ${LINUX}; then
     alias display_time='/usr/bin/time -f "[INFO] Return code : %x\n[INFO] Time report (sec) : \t%e real,\t%U user,\t%S system"'
   else
