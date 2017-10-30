53215b6c6940c6e29e4bfc3317039fe7efe65731
Frederic DROUET <fdrouet@exoplatform.com>
2017-10-30 12:26:00 +0100

    ACC-83 : allow platform.log file in log reader

--- var/www/lib/functions.php
+++ var/www/lib/functions.php
@@ -827,11 +827,16 @@ function checkCaches()
 function isAuthorizedToReadFile($log_type, $file_path)
 {
   if (!empty($log_type) && !is_null($log_type)){
-      if ($log_type == "instance" && (strpos($file_path, 'catalina.out') !== false
-            || strpos($file_path, 'server.log') !== false || strpos($file_path, 'access.log') !== false)){
-        return true;
-      }
-      return false;
+      if ($log_type == "instance" && (
+        strpos($file_path, 'catalina.out') !== false
+        || strpos($file_path, 'platform.log') !== false 
+        || strpos($file_path, 'server.log') !== false 
+        || strpos($file_path, 'access.log') !== false
+        )
+      ){
+      return true;
+    }
+    return false;
   }
   return false;
 }
