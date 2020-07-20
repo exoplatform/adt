d45cce6ca968e207e1d103777bd1b2f3b84f19bc
hbenali <hbenali@exoplatform.com>
2020-07-20 23:06:25 +0200

    Fix showing Not authorized to read this file for apache2 logs

--- var/www/lib/functions.php
+++ var/www/lib/functions.php
@@ -1078,16 +1078,8 @@ function checkCaches()
 function isAuthorizedToReadFile($log_type, $file_path)
 {
   if (!empty($log_type) && !is_null($log_type)){
-      if ($log_type == "instance" && (
-        strpos($file_path, 'catalina.out') !== false
-        || strpos($file_path, 'platform.log') !== false 
-        || strpos($file_path, 'server.log') !== false 
-        || strpos($file_path, 'access.log') !== false
-        )
-      ){
-      return true;
-    }
-    return false;
+    return(($log_type == "instance" && preg_match('/(platform.log|server.log|catalina.out)$/', $file_path, $match))
+    || ($log_type == "apache" && preg_match('/access.log$/', $file_path, $match)));
   }
   return false;
 }
