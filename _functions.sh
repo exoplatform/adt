1d39bd96935e5803ecbfac9c665bf64004f821a8
Houssem Ben Ali <hbenali@exoplatform.com>
2021-06-01 15:13:49 +0100

    Bump mysql driver version to 8.0.25

--- _functions.sh
+++ _functions.sh
@@ -769,17 +769,17 @@ initialize_product_settings() {
         if [[ "${PRODUCT_VERSION}" =~ ^(1.1|1.2) ]]; then
               env_var "DEPLOYMENT_FORCE_JDBC_DRIVER_ADDON" "false"
               env_var "DEPLOYMENT_ES_IMAGE_VERSION" "1.2.2"
-              env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.19" # Default version of the mysql server to use
+              env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.25" # Default version of the mysql server to use
               env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "13" # Default version of the postgresql server to use
               env_var "DEPLOYMENT_POSTGRESQL_DRIVER_VERSION" "42.2.18"
-              env_var "DEPLOYMENT_MYSQL_DRIVER_VERSION" "8.0.18"
+              env_var "DEPLOYMENT_MYSQL_DRIVER_VERSION" "8.0.25"
         elif [[ "${PRODUCT_VERSION}" =~ ^(1.0) ]]; then
               env_var "DEPLOYMENT_FORCE_JDBC_DRIVER_ADDON" "false"
               env_var "DEPLOYMENT_ES_IMAGE_VERSION" "1.2.2"
-              env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.19" # Default version of the mysql server to use
+              env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.25" # Default version of the mysql server to use
               env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "11" # Default version of the postgresql server to use
               env_var "DEPLOYMENT_POSTGRESQL_DRIVER_VERSION" "42.2.10"
-              env_var "DEPLOYMENT_MYSQL_DRIVER_VERSION" "8.0.18"
+              env_var "DEPLOYMENT_MYSQL_DRIVER_VERSION" "8.0.25"
         else 
               echo_error "Product version \"${PRODUCT_VERSION}\" not yet managed"
               exit 1
