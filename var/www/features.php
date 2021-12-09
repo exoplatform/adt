8c5c89b82cde46f4808c4d3701369eae8c5955df
Houssem Ben Ali <benalihoussem@yandex.com>
2021-12-09 17:23:36 +0100

    Revert "Hide Uneeded views of Feature Branches"
    This reverts commit 809e2faa30abb9c56b4c051b3aa86ad794b52531.

--- var/www/features.php
+++ var/www/features.php
@@ -31,7 +31,7 @@ checkCaches();
                     <p>This page summarizes all Git feature branches (<code>feature/.*</code>) and try to give an overview of branches health.</p>
 
                     <h3>Feature Branches <u>deployed on acceptance</u> <span class="subtitle">(status compared to each project <code>develop</code> branch.)</span></h3>
-                    <!--<?php
+                    <?php
                     //List all projects
                     $projectsNames = getRepositories();
                     $projects = array_keys(getRepositories());
@@ -95,7 +95,7 @@ checkCaches();
                         <?php } ?>
                         </tbody>
                     </table>
-                    <h3>Others branches ... <span class="subtitle">(status compared to each project <code>develop</code> branch.)</span><br/>ARE YOU SURE YOU DON'T NEED TO DO SOME BRANCH CLEANUP ? </h3>-->
+                    <h3>Others branches ... <span class="subtitle">(status compared to each project <code>develop</code> branch.)</span><br/>ARE YOU SURE YOU DON'T NEED TO DO SOME BRANCH CLEANUP ? </h3>
                     <table class="table  table-hover table-header-rotated">
                         <thead>
                         <tr>
