ad5b9f4320ce5ff7f5edfb2e04902d146fd692e1
Houssem Ben Ali <hbenali@exoplatform.com>
2022-02-10 23:40:02 +0100

    Make permessive

--- _functions_es.sh
+++ _functions_es.sh
@@ -245,7 +245,7 @@ do_restore_es_dataset() {
     sudo chown 1000.1000 -R ${path}
   else
     local mount_point=$(${DOCKER_CMD} volume inspect --format '{{ .Mountpoint }}' ${DEPLOYMENT_ES_CONTAINER_NAME})
-    sudo mv -v ${_esData}/* ${mount_point}/ >/dev/null
+    sudo mv -v ${_esData}/* ${mount_point}/ || true >/dev/null
     sudo chown 1000.1000 -R ${mount_point}
   fi
   rm -rf ${_esData}
@@ -257,10 +257,10 @@ do_dump_es_dataset() {
   mkdir -p ${_esData}
   if ${DEPLOYMENT_ES_EMBEDDED}; then
     local path="${DEPLOYMENT_DIR}/${DEPLOYMENT_ES_PATH_DATA}"
-    cp -r ${path}/* ${_esData}/
+    cp -r ${path}/* ${_esData}/ || touch ${_esData}/__nofile
   else
     local mount_point=$(${DOCKER_CMD} volume inspect --format '{{ .Mountpoint }}' ${DEPLOYMENT_ES_CONTAINER_NAME})
-    sudo cp -r ${mount_point}/* ${_esData}/ || true
+    sudo cp -r ${mount_point}/* ${_esData}/ || touch ${_esData}/__nofile
   fi
 }
 
