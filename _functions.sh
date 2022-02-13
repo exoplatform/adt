623c1227964df47f5078e95470a8ce0a1e14c068
Houssem Ben Ali <hbenali@exoplatform.com>
2022-02-13 13:56:37 +0100

    Restore mysql jdbc for meeds 1.2

--- _functions.sh
+++ _functions.sh
@@ -812,7 +812,7 @@ initialize_product_settings() {
         elif [[ "${PRODUCT_VERSION}" =~ ^(1.2) ]]; then
               env_var "DEPLOYMENT_FORCE_JDBC_DRIVER_ADDON" "false"
               env_var "DEPLOYMENT_ES_IMAGE_VERSION" "2.0.2"
-              env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.28" # Default version of the mysql server to use
+              env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.21" # Default version of the mysql server to use
               env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "13" # Default version of the postgresql server to use
               env_var "DEPLOYMENT_POSTGRESQL_DRIVER_VERSION" "42.2.18"
               env_var "DEPLOYMENT_MYSQL_DRIVER_VERSION" "8.0.28"
@@ -932,7 +932,7 @@ initialize_product_settings() {
               env_var "DEPLOYMENT_ES_IMAGE_VERSION" "2.0.2"
               env_var "DEPLOYMENT_CHAT_MONGODB_VERSION" "4.2"
 
-              env_var "DEPLOYMENT_MYSQL_ADDON_VERSION" "2.0.1" # Default version of the mysql driver addon to use
+              env_var "DEPLOYMENT_MYSQL_ADDON_VERSION" "2.0.2" # Default version of the mysql driver addon to use
               env_var "DEPLOYMENT_POSTGRESQL_ADDON_VERSION" "2.1.0" # Default version of the jdbc postgresql driver addon to use
 
               env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.28" # Default version of the mysql server to use
