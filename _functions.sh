c93c17bfc9eef214abb78f7b54b50290ac807f74
Houssem Ben Ali <hbenali@exoplatform.com>
2022-04-17 11:32:53 +0200

    TASK-55879: Upgrade Postgres to 14 for PLF 6.3.x / Meeds 1.3.x (#157)

--- _functions.sh
+++ _functions.sh
@@ -810,8 +810,8 @@ initialize_product_settings() {
               env_var "DEPLOYMENT_FORCE_JDBC_DRIVER_ADDON" "false"
               env_var "DEPLOYMENT_ES_IMAGE_VERSION" "2.0.3"
               env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.28" # Default version of the mysql server to use
-              env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "13" # Default version of the postgresql server to use
-              env_var "DEPLOYMENT_POSTGRESQL_DRIVER_VERSION" "42.2.18"
+              env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "14" # Default version of the postgresql server to use
+              env_var "DEPLOYMENT_POSTGRESQL_DRIVER_VERSION" "42.3.3"
               env_var "DEPLOYMENT_MYSQL_DRIVER_VERSION" "8.0.28"
         elif [[ "${PRODUCT_VERSION}" =~ ^(1.2) ]]; then
               env_var "DEPLOYMENT_FORCE_JDBC_DRIVER_ADDON" "false"
@@ -924,10 +924,10 @@ initialize_product_settings() {
               env_var "DEPLOYMENT_CHAT_MONGODB_VERSION" "4.4"
 
               env_var "DEPLOYMENT_MYSQL_ADDON_VERSION" "2.0.3" # Default version of the mysql driver addon to use
-              env_var "DEPLOYMENT_POSTGRESQL_ADDON_VERSION" "2.1.0" # Default version of the jdbc postgresql driver addon to use
+              env_var "DEPLOYMENT_POSTGRESQL_ADDON_VERSION" "2.2.0" # Default version of the jdbc postgresql driver addon to use
 
               env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.28" # Default version of the mysql server to use
-              env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "13" # Default version of the postgresql server to use
+              env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "14" # Default version of the postgresql server to use
               
               # TO DO Once onlyoffice/documentserver-ie:6.1 is released, switch to that image and use a fixed version
               configurable_env_var "DEPLOYMENT_ONLYOFFICE_IMAGE" "onlyoffice/documentserver"
