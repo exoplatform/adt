807720367ed1df9d1ac87325ffdcaf4db9bd898d
Houssem Ben Ali <hbenali@exoplatform.com>
2023-01-31 17:15:02 +0100

    Add PHP

--- var/www/lib/functions-ui.php
+++ var/www/lib/functions-ui.php
@@ -113,6 +113,9 @@ function buildTableTitleDev($plf_branch) {
     case "1.4.x":
       $content="Platform " . $plf_branch . " based builds (Meeds)";
       break;
+    case "1.5.x":
+      $content="Platform " . $plf_branch . " based builds (Meeds)";
+      break;
     case "4.0.x":
     case "4.1.x":
     case "4.2.x":
@@ -139,6 +142,9 @@ function buildTableTitleDev($plf_branch) {
       $content="Platform " . $plf_branch . " based builds (Maintenance)";
       break;  
     case "6.4.x":
+      $content="Platform " . $plf_branch . " based builds (Maintenance)";
+      break;  
+    case "6.5.x":
       $content="Platform " . $plf_branch . " based builds (R&D) - next product release (no date yet)";
       break;  
     case "5.x":
