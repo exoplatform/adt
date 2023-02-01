220deb319554f33282c2bf94fd46ef24eb65c4ea
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-02-01 10:36:21 +0100

    Upgrade Postgres to 15 for PLF 6.4+

--- _functions.sh
+++ _functions.sh
@@ -815,7 +815,7 @@ initialize_product_settings() {
               env_var "DEPLOYMENT_FORCE_JDBC_DRIVER_ADDON" "true"
               env_var "DEPLOYMENT_ES_IMAGE_VERSION" "2.0.3"
               env_var "DEPLOYMENT_MYSQL_DEFAULT_VERSION" "8.0.31" # Default version of the mysql server to use
-              env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "14" # Default version of the postgresql server to use
+              env_var "DEPLOYMENT_POSTGRESQL_DEFAULT_VERSION" "15" # Default version of the postgresql server to use
               env_var "DEPLOYMENT_MYSQL_ADDON_VERSION" "2.0.4" # Default version of the mysql driver addon to use
               env_var "DEPLOYMENT_POSTGRESQL_ADDON_VERSION" "2.3.0" # Default version of the jdbc postgresql driver addon to use
         elif [[ "${PRODUCT_VERSION}" =~ ^(1.3) ]]; then
