b391421bcbcc82307c405225934c9343d9a59eb5
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-09-15 18:10:47 +0200

    Add staging catalog duplicates calculation

--- var/www/rest/local-catalog.php
+++ var/www/rest/local-catalog.php
@@ -1,9 +1,21 @@
 <?php 
+
+function searchDuplicateCatalog($arr, $obj) {
+    foreach ($arr as $value) {
+        if ($value['id'] == $obj['id'] && $value['version'] == $obj['version']) {
+            return true;
+        }
+    }
+    return false;
+};
+
 if(!isset($_GET["plfversion"])){
     http_response_code(404);
     echo "Error! Missing PLF version parameter!";
     die();
 }
+$stagingCatalog = isset($_GET["staging"]) && $_GET["staging"] === 'true'; 
+$targetCatalog = $stagingCatalog ? 'staging' : 'local';
 // Continous deployment version check
 if(!preg_match('/^[0-9].[0-9].[0-9](-(exo|meed))?-[0-9]{8,10}$/', $_GET["plfversion"])){
     http_response_code(404);
@@ -14,15 +26,15 @@ $exo_plf_major_version="";
 if ( preg_match('/^[0-9].[0-9]/', $_GET["plfversion"], $matches) ) {
     $exo_plf_major_version=$matches[1];
 }
-if (!file_exists("../catalog/".$exo_plf_major_version."/local_catalog.json")) {
+if (!file_exists("../catalog/".$exo_plf_major_version."/".$targetCatalog."_catalog.json")) {
     http_response_code(404);
-    echo "Error! Could not find local catalog file for ".$exo_plf_major_version." version!";
+    echo "Error! Could not find ".$targetCatalog." catalog file for ".$exo_plf_major_version." version!";
     die();
 }
-$local_catalog = file_get_contents("../catalog/".$exo_plf_major_version."/local_catalog.json");
+$local_catalog = file_get_contents("../catalog/".$exo_plf_major_version."/".$targetCatalog."_catalog.json");
 if ($local_catalog === false) {
     http_response_code(500);
-    echo "Error! Could not parse local catalog file!";
+    echo "Error! Could not parse ".$targetCatalog." catalog file!";
     die();
 }
 $plf_version=$_GET["plfversion"];
@@ -35,6 +47,17 @@ if(isset($_GET["remote"])){
     $remote_catalog = file_get_contents("https://www.exoplatform.com/addons/catalog");
 }
 header("Content-type: application/json; charset=utf-8");
-// Merge remote catalog and local catalog and print result
-echo str_replace("\/","/",json_encode(array_merge(json_decode($local_catalog, true),json_decode($remote_catalog, true)),JSON_PRETTY_PRINT));
+// Merge remote catalog and local catalog and print result; Merge order is crucial for staging catalog duplicates removal.
+$mergedArray=array_merge(json_decode($local_catalog, true),json_decode($remote_catalog, true));
+if ($stagingCatalog) {
+    $uniqueArray = array();
+    foreach ($mergedArray as $obj) {
+        if (searchDuplicateCatalog($uniqueArray, $obj) === false) {
+            $uniqueArray[] = $obj;
+        }
+    }
+    echo str_replace("\/","/",json_encode($uniqueArray,JSON_PRETTY_PRINT));
+} else {
+    echo str_replace("\/","/",json_encode($mergedArray,JSON_PRETTY_PRINT));
+}
 ?>
