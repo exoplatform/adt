fc93c1b12983203b0d89e9b4c23c1ae76e9c2d41
Houssem Ben Ali <hbenali@exoplatform.com>
2023-10-10 09:56:17 +0100

    Add custom logback loggers support (#203)
    * Add custom logback loggers support
    
    * Update _functions.sh
    
    Co-authored-by: Omar Bouras <obouras@exoplatform.com>
    
    ---------
    
    Co-authored-by: Omar Bouras <obouras@exoplatform.com>

--- _functions.sh
+++ _functions.sh
@@ -189,6 +189,8 @@ Environment Variables
   DEPLOYMENT_ES_EMBEDDED_MIGRATION_ENABLED  : Enable elastic serach migration from embedded to standalone
   DEPLOYMENT_ES7_MIGRATION_ENABLED  : Enable elastic serach migration to version 7
   DEPLOYMENT_GZIP_ENABLED           : Enable Gzip Compression on the Tomcat Server
+  DEPLOYMENT_LOGBACK_LOGGERS  : Enable Debug logging for java packages (comma seperated)
+  DEPLOYMENT_LOGBACK_LOGGERS_LEVEL  : Customize Logback logging level (INFO, DEBUG, ERROR): Default: DEBUG
   DEPLOYMENT_UPLOAD_MAX_FILE_SIZE   : Configure the max size for file upload in eXo (in MB)
 
 EOF
@@ -364,6 +366,8 @@ initialize_product_settings() {
       
       configurable_env_var "DEPLOYMENT_ES7_MIGRATION_ENABLED" false
       configurable_env_var "DEPLOYMENT_GZIP_ENABLED" true
+      configurable_env_var "DEPLOYMENT_LOGBACK_LOGGERS" ""
+      configurable_env_var "DEPLOYMENT_LOGBACK_LOGGERS_LEVEL" "DEBUG"
       configurable_env_var "DEPLOYMENT_UPLOAD_MAX_FILE_SIZE" "200"
       configurable_env_var "DS_FILENAME" "${PRODUCT_NAME}-${PRODUCT_BRANCH}"
       configurable_env_var "DS_TARGET_SERVER" ""
