14a73e72378074fc449522a2a951abae6eea4582
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-10-23 23:31:36 +0200

    Bump cloudbeaver to 23.2.3 (1.3.2)

--- _functions.sh
+++ _functions.sh
@@ -338,7 +338,7 @@ initialize_product_settings() {
 
       configurable_env_var "DEPLOYMENT_CLOUDBEAVER_ENABLED" false
       configurable_env_var "DEPLOYMENT_CLOUDBEAVER_IMAGE" "exoplatform/cloudbeaver"
-      configurable_env_var "DEPLOYMENT_CLOUDBEAVER_IMAGE_VERSION" "1.3.0-acc"
+      configurable_env_var "DEPLOYMENT_CLOUDBEAVER_IMAGE_VERSION" "1.3.2-acc"
       configurable_env_var "DEPLOYMENT_CLOUDBEAVER_READONLY" true
 
       configurable_env_var "DEPLOYMENT_PHPLDAPADMIN_ENABLED" false
