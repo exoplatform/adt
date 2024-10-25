0f03157ecca4c4154da2dd1dc6a3cb8cedd11be6
ayoub cherif <aycherif@exoplatform.com>
2024-10-25 14:34:37 +0100

    Remove env file for homeserver file

--- _functions_matrix.sh
+++ _functions_matrix.sh
@@ -104,7 +104,6 @@ do_start_matrix() {
     -v ${DEPLOYMENT_DIR}/matrix.host.signing.key:/data/matrix.host.signing.key:ro \
     -p "${DEPLOYMENT_MATRIX_HTTP_PORT}:8008" \
     -p "${DEPLOYMENT_MATRIX_HTTPS_PORT}:8448" \
-    --env-file ${DEPLOYMENT_DIR}/homeserver.yaml \
     --health-cmd="curl -fSs http://localhost:8008/health || exit 1" \
     --health-interval=15s \
     --health-timeout=5s \
