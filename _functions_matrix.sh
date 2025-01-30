73818c1da2cde63953bbd6d0703e845973e48e5a
ayoub cherif <aycherif@exoplatform.com>
2025-01-30 12:40:02 +0100

    Redirect & configure Matrix logs in ADT

--- _functions_matrix.sh
+++ _functions_matrix.sh
@@ -61,8 +61,8 @@ do_start_matrix() {
     return
   fi
   mkdir -p ${DEPLOYMENT_DIR}/matrix
-  mkdir -p ${DEPLOYMENT_DIR}/logs/matrix
-  chown -R 991:991 ${DEPLOYMENT_DIR}/logs/matrix
+  mkdir -p ${DEPLOYMENT_DIR}/matrix/logs
+  sudo chown -R 991:991 ${DEPLOYMENT_DIR}/matrix/logs
   evaluate_file_content ${ETC_DIR}/matrix/homeserver.yaml.template ${DEPLOYMENT_DIR}/matrix/homeserver.yaml
   evaluate_file_content ${ETC_DIR}/matrix/initialize.sh.template ${DEPLOYMENT_DIR}/matrix/initialize.sh
   chmod +x ${DEPLOYMENT_DIR}/matrix/initialize.sh
@@ -84,7 +84,7 @@ do_start_matrix() {
     -d \
     -v ${DEPLOYMENT_DIR}/matrix/homeserver.yaml:/data/homeserver.yaml:ro \
     -v ${DEPLOYMENT_DIR}/matrix/matrix.host.signing.key:/data/matrix.host.signing.key:ro \
-    -v ${DEPLOYMENT_DIR}/logs/matrix:/var/log/matrix \
+    -v ${DEPLOYMENT_DIR}/matrix/logs:/var/log/matrix \
     -v ${DEPLOYMENT_DIR}/matrix/matrix.log.config:/data/matrix.log.config:ro \
     -v ${DEPLOYMENT_DIR}/matrix/media_store:/data/media_store \
     -v ${DEPLOYMENT_MATRIX_CONTAINER_NAME}_data:/data:rw \
