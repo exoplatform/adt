420150fd788cd0f596b16b286aec275f2423d8bd
ayoub cherif <aycherif@exoplatform.com>
2025-02-05 17:53:54 +0100

    Cleanup config after it works

--- _functions_matrix.sh
+++ _functions_matrix.sh
@@ -77,8 +77,6 @@ do_start_matrix() {
   #Change Matrix data directory to 991
   docker run --rm -v ${DEPLOYMENT_MATRIX_CONTAINER_NAME}_data:/data alpine \
   sh -c "chown -R 12000:12000 /data"
-#  docker run --rm -v ${DEPLOYMENT_DIR}/logs:/var/log/matrix alpine \
-#  sh -c "chown -R 991:991 /var/log/matrix"
 
   local SMTP_SERVER='0.0.0.0'
   if [ "${DEPLOYMENT_MAILPIT_ENABLED}" == "true" ]; then
