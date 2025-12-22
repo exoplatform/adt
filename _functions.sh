4b5721a0ff1a5315296d963fef807969a0e377c2
ayoub cherif <aycherif@exoplatform.com>
2025-12-22 13:01:33 +0100

    make cloudbeaver write

--- _functions.sh
+++ _functions.sh
@@ -376,7 +376,7 @@ initialize_product_settings() {
       configurable_env_var "DEPLOYMENT_CLOUDBEAVER_ENABLED" false
       configurable_env_var "DEPLOYMENT_CLOUDBEAVER_IMAGE" "exoplatform/cloudbeaver"
       configurable_env_var "DEPLOYMENT_CLOUDBEAVER_IMAGE_VERSION" "25.3.1-acc"
-      configurable_env_var "DEPLOYMENT_CLOUDBEAVER_READONLY" true
+      configurable_env_var "DEPLOYMENT_CLOUDBEAVER_READONLY" false
 
       configurable_env_var "DEPLOYMENT_PHPLDAPADMIN_ENABLED" false
       configurable_env_var "DEPLOYMENT_PHPLDAPADMIN_IMAGE" "rschaeuble/phpldapadmin"
